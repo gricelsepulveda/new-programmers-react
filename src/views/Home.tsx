606f80c5bb9f20c7f01a183cab5602ad54b01d33
Gricel <hello@gricel.dev>
2021-01-24 20:59:35 -0300

    First stone

---
+++ src/views/Home.tsx
@@ -0,0 +1,16 @@
+//IMPORTS
+import React from "react"
+
+//TYPES
+
+const Home = () => {
+    //STATES
+    //FUNCTIONS
+    return (
+        <>
+            {/* Your html*/}
+        </>
+    )
+}
+
+export default Home
